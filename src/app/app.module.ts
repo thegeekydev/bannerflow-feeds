4cbe67d9bbc5c780686d7f892a469af7e9b714f7
TheGeekyDev <morteza@thegeeky.dev>
2019-12-30 23:08:32 +0330

    added core components(header,sidebar) and added those in the app layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { appRoutes } from './routes';
-import { AppComponent } from './app.component';
-import { FeedsComponent } from './feeds/feeds.component';
-import { FeedsModule } from './feeds/feeds.module';
+import { appRoutes } from "./routes";
+import { AppComponent } from "./app.component";
+import { FeedsComponent } from "./feeds/feeds.component";
+import { FeedsModule } from "./feeds/feeds.module";
+import { CoreModule } from "./core/core.module";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FeedsComponent
-  ],
+  declarations: [AppComponent, FeedsComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes, {useHash: true}),
+    RouterModule.forRoot(appRoutes, { useHash: true }),
     FeedsModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+
+import { HeaderComponent } from "./header/header.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+@NgModule({
+  imports: [CommonModule, RouterModule],
+  exports: [HeaderComponent, SidebarComponent],
+  declarations: [HeaderComponent, SidebarComponent],
+  providers: []
+})
+export class CoreModule {}
